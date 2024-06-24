985e911b1376c714111643cf09e84284243511dc
lejlamrt <lejlamuratovic6@gmail.com>
2024-06-24 19:13:13 +0200

    frontend setup

---
+++ frontend/src/scripts/initializeWeb3.js
@@ -0,0 +1,14 @@
+import Web3 from "web3";
+
+let web3;
+
+const initializeWeb3 = () => {
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    return web3;
+  } else {
+    alert("Please install MetaMask!");
+  }
+};
+
+export default initializeWeb3;
