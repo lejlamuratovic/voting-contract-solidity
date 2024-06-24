8ff32bee8b8a9b02d3fcf98ec31ee66d914b67f3
Lejla Muratović <114357846+lejlamuratovic@users.noreply.github.com>
2024-06-24 18:53:12 +0200

    Initial smart contract (#1)

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+};
